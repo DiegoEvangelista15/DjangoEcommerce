1b4b00087dd4f6a00a888e538b17fadb1106350e
DiegoEvangelista15 <diegoapevangelista@gmail.com>
2022-06-12 17:35:18 -0300

    flake8 and tests adjusments

--- store/tests/test_views.py
+++ store/tests/test_views.py
@@ -1,10 +1,13 @@
 from unittest import skip
-from django.test import TestCase
+
+# from django.test import TestCase
 from django.contrib.auth.models import User
-from store.models import Category, Product
+from django.http import HttpRequest
+from django.test import Client, RequestFactory, TestCase  # advance tests
 from django.urls import reverse
 
-from django.test import Client
+from store.models import Category, Product
+from store.views import all_products
 
 # coverage run --omit='*/venv/*' manage.py test - use for test
 
@@ -17,6 +20,7 @@ from django.test import Client
 class TestViewsResponses(TestCase):
     def setUp(self) -> None:
         self.c = Client()
+        self.factory = RequestFactory()
         Category.objects.create(name='Django', slug='django')
         User.objects.create(username='admin')
         Product.objects.create(category_id=1, title='django beginners', created_by_id=1, slug='django-beginners', price=20.00, image='django')
@@ -33,3 +37,18 @@ class TestViewsResponses(TestCase):
         response = self.c.get(reverse('store:category_list', args=['django']))
         self.assertEqual(response.status_code, 200)
         
+    def test_homepage_html(self):
+        request = HttpRequest()
+        response = all_products(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<title>Home</title>', html)
+        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_view_function(self):
+        request = self.factory.get('item/django-beginners')
+        response = all_products(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<title>Home</title>', html)
+        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
+        self.assertEqual(response.status_code, 200)
