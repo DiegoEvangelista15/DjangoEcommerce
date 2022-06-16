0237ec22cdd44499d8c60f3a01f5432f2002f366
DiegoEvangelista15 <diegoapevangelista@gmail.com>
2022-06-16 18:33:32 -0300

    added button-jsfunction

--- basket/basket.py
+++ basket/basket.py
@@ -8,4 +8,13 @@ class Basket():
         if 'skey' not in request.session:
             basket = self.session.get['skey']  = {}
         self.basket = basket
+        
+    def add(self, product, qty):
+        product_id = product.id
+        
+        if product_id not in self.basket:
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
             
+        self.session.modified = True
+            
+    
