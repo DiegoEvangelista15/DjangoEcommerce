7f527f8f9b0eb50960b7184e7299f3361968c687
DiegoEvangelista15 <diegoapevangelista@gmail.com>
2022-06-16 19:56:33 -0300

    cart with update and delete(review)

--- basket/basket.py
+++ basket/basket.py
@@ -1,4 +1,6 @@
+from decimal import Decimal
 
+from store.models import Product    
 
 class Basket():
     
@@ -17,4 +19,42 @@ class Basket():
             
         self.session.modified = True
             
-    def po
+    def __iter__(self):
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item          
+
+
+
+    def __len__(self):
+        return sum(item['qty'] for item in self.basket.values())
+    
+    def update(self, product, qty):
+       
+        product_id = str(product)
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+        self.save()
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
+
+    def delete(self, product):
+        
+        product_id = str(product)
+
+        if product_id in self.basket:
+            del self.basket[product_id]
+            print(product_id)
+            self.save()
+
+    def save(self):
+        self.session.modified = True
--- basket/views.py
+++ basket/views.py
@@ -6,7 +6,8 @@ from store.models import Product
 # Create your views here.
 
 def basket_summary(request):
-    return render(request, 'summary.html' )
+    basket = Basket(request)
+    return render(request, 'summary.html', {'basket': basket} )
 
 def basket_add(request):
     basket = Basket(request)
@@ -15,6 +16,32 @@ def basket_add(request):
         product_qty = int(request.POST.get('productqty'))
         product =  get_object_or_404(Product, id=product_id)
         basket.add(product=product, qty=product_qty)
-        response = JsonResponse({'qty': product_qty})
+        
+        basketqty = basket.__len__()
+        response = JsonResponse({'qty': basketqty})
+        return response
+
+def basket_delete(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        basket.delete(product=product_id)
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
+        return response
+
+
+def basket_update(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        basket.update(product=product_id, qty=product_qty)
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
         return response
     
